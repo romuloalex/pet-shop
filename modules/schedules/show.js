d78f24010db731a84f7528ba7d9adb71cc25692a
Rômulo Alexandre <romulobier@gmail.com>
2025-10-24 08:42:29 -0300

    Update show.js

--- modules/schedules/show.js
+++ modules/schedules/show.js
@@ -18,14 +18,18 @@ export function scheduleShow({ dailySchedules }) {
             //Extrai a hora do agendamento para classificação de turno
             const scheduleHour = dayjs(schedule.when).hour()
 
-            // Determinar qual o destino referente ao truno
-            let targelist
-            if(scheduleHour >= 9 && scheduleHour <= 12) {
-                targelist = periodMorning
-            } else if(scheduleHour >= 13 && scheduleHour <= 18) {
-                targelist = periodAfternoon
-            } else {
-                targelist = periodNight
+            // --- CORREÇÃO: Lógica de classificação de turno ---
+            // --- CORREÇÃO: Erro de digitação (targelist -> targetList) ---
+            let targetList // <-- Corrigido
+
+            // Lógica corrigida para não classificar 8h como "Noite"
+            // (Ajuste os horários 12 e 18 se a regra de negócio for diferente)
+            if(scheduleHour < 12) { // Manhã: (00:00 - 11:59)
+                targetList = periodMorning
+            } else if(scheduleHour >= 12 && scheduleHour < 18) { // Tarde: (12:00 - 17:59)
+                targetList = periodAfternoon
+            } else { // Noite: (18:00 - 23:59)
+                targetList = periodNight
             }
 
             //Criar um elemento LI
@@ -35,17 +39,17 @@ export function scheduleShow({ dailySchedules }) {
 
             //Definir o innerHTML da tag LI com as informações do agendamento
             liElement.innerHTML = `
-                                <div class="col-lg-5 col-12 hours-name-group d-flex gap-3 align-items-lg-center">
-                                    <span class="hours">${dayjs(schedule.when).format("HH:mm")}</span>
-                                    <p class="name"><span>${schedule.pet}</span> / ${schedule.name}</p>
-                                </div>
-                                <div class="col-lg-4 col-12 service ms-lg-auto">${schedule.description}</div>
-                                <div class="col-lg-3 col-12 remove text-end">
-                                    <a href="#" class="remove-schedule">Remover agendamento</a>
-                                </div>
-            
+                        <div class="col-lg-5 col-12 hours-name-group d-flex gap-3 align-items-lg-center">
+                            <span class="hours">${dayjs(schedule.when).format("HH:mm")}</span>
+                            <p class="name"><span>${schedule.pet}</span> / ${schedule.name}</p>
+                        </div>
+                        <div class="col-lg-4 col-12 service ms-lg-auto">${schedule.description}</div>
+                        <div class="col-lg-3 col-12 remove text-end">
+                            <a href="#" class="remove-schedule">Remover agendamento</a>
+                        </div>
             `
-            targelist.appendChild(liElement)
+            
+            targetList.appendChild(liElement) // <-- Corrigido
         })
 
     } catch (error) {
